3a662d123e16a7cd7e42b21fdfca91dbd9cb1cd4
shreyasiddheshwar12 <166814221+shreyasiddheshwar12@users.noreply.github.com>
2026-09-14 21:14:57 +0530

    Add offline PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='accountability-league-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html')))));
